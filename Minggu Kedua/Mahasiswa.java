ae544958a046e566940752901c56d38682ea2041
Mohamad Fathan Mustaqim <fathanf09@gmail.com>
2017-02-28 15:32:42 +0700

    Add files via upload

---
+++ Minggu Kedua/Mahasiswa.java
@@ -0,0 +1,10 @@
+/*Tugas minggu ke 2*/
+class Mahasiswa
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Nama Lengkap : Mohamad Fathan Mustaqim\n");
+		System.out.println("NIM          : 10115264\n");
+		System.out.println("Alamat       : Tasikmalaya.");
+	}
+}
