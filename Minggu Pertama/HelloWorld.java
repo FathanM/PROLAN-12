1aa79f984f1b3f3c8f84806198f4f79d6bce9bf8
Mohamad Fathan Mustaqim <fathanf09@gmail.com>
2017-02-28 15:31:59 +0700

    Add files via upload

---
+++ Minggu Pertama/HelloWorld.java
@@ -0,0 +1,6 @@
+/*Program pertama HelloworLd*/
+class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		}
+}
---
+++ Minggu Pertama/program_netbean/HelloWrold1/src/HelloWrold/java/HelloWrold1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HelloWrold.java;
+
+/**
+ *
+ * @author Fays
+ */
+public class HelloWrold1 {
+    /*Program pertama Helloword*/
+	public static void main(String[] args) 
+        {
+		System.out.println("Hello world");
+        }
+}
