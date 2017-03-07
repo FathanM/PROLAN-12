acbe8f09a26295f6368991d382344bdb4e517242
Mohamad Fathan Mustaqim <fathanf09@gmail.com>
2017-03-07 22:53:08 +0700

    Add files via upload

---
+++ Minggu Ketiga/Hotel/mainhotel.java
@@ -0,0 +1,9 @@
+public class mainhotel
+{
+     public static void main(String[]args)
+	 {
+		 resepsionis fathan = new resepsionis();
+	     fathan.Nama = "Mohamad Fathan Mustaqim";
+		 fathan.Melapor_Aktivitas();
+	 }
+}
---
+++ Minggu Ketiga/Hotel/resepsionis.java
@@ -0,0 +1,23 @@
+class resepsionis
+{
+   public String Nama; 
+   String Tgl_lahir, ID;
+   int Umur;
+   char JenisKel;
+   
+   void Menerima_Tamu()
+   {
+   
+   }
+   
+   void Mencatat_data_Tamu()
+   {
+   
+   }
+   
+   void Melapor_Aktivitas()
+   {
+          System.out.println("Saudara Fathan dilaporkan atas tidak membayar biaya penginapan");
+   }
+     
+}
