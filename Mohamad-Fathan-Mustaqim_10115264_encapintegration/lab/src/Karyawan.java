29f628e960f1a1130b8d32b4489f32ba9f427585
Mohamad Fathan Mustaqim <fathanf09@gmail.com>
2017-05-17 22:26:59 +0700

    Add files via upload

---
+++ Mohamad-Fathan-Mustaqim_10115264_encapintegration/lab/src/Karyawan.java
@@ -0,0 +1,36 @@
+public class Karyawan {
+    private String nama, pekerjaan, alamat;
+    private int umur;
+    
+    public String getNama(){
+        return nama;
+    }
+   
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+    public String getPekerjaan(){
+        return pekerjaan;
+    }
+    
+    public void setPekerjaan(String pekerjaan){
+        this.pekerjaan = pekerjaan;
+    }
+    
+    public String getAlamat(){
+        return alamat;
+    }
+    
+    public void setAlamat(String alamat){
+        this.alamat = alamat;
+    }
+    public int getUmur(){
+        return umur;
+    }
+    
+    public void setUmur(int umur){
+        this.umur = umur;
+    }
+    
+}
